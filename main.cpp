f0b42887278e6a5c21240bf35993fb259fc19ddf
RIvance <ivance@foxmail.com>
2022-04-27 04:32:44 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,402 @@
+#include <opencv2/opencv.hpp>
+#include <eigen3/Eigen/Eigen>
+
+#include <cmath>
+#include <random>
+#include <utility>
+
+using SysClock = std::chrono::system_clock;
+using Timestamp = SysClock::time_point;
+template <typename T> using Duration = std::chrono::duration<T>;
+template <typename T> using Vector = std::vector<T>;
+
+struct GraphicLine
+{
+    cv::Point p1, p2;
+    int label;
+};
+
+struct GeometryLine
+{
+    Eigen::Vector2d p1, p2;
+    int label = 0;
+
+    GraphicLine graphic(cv::Size imageSize) const
+    {
+        return {
+            cv::Point(
+                (int) p1(0) + imageSize.width / 2,
+                (int) -p1(1) + imageSize.height / 2
+            ),
+            cv::Point(
+                (int) p2(0) + imageSize.width / 2,
+                (int) -p2(1) + imageSize.height / 2
+            ),
+            label
+        };
+    }
+
+    GeometryLine transform(const Eigen::Matrix2d & mat) const
+    {
+        return GeometryLine(mat * p1, mat * p2, label);
+    }
+
+    GeometryLine() = default;
+
+    explicit GeometryLine(Eigen::Vector2d p1, Eigen::Vector2d p2, int label)
+        : p1(std::move(p1)), p2(std::move(p2)), label(label)
+    { /* empty */ }
+
+    explicit GeometryLine(
+        double x1, double y1, double x2, double y2,
+        int label = 0
+    ) {
+        p1 << x1, y1;
+        p2 << x2, y2;
+        this->label = label;
+    }
+};
+
+class Geometry
+{
+  private:
+
+    Vector<GeometryLine> lines;
+
+  public:
+
+    void rotate(double angle)
+    {
+        Eigen::Matrix2d rotateMatrix;
+        rotateMatrix << std::cos(angle), std::sin(angle),
+                        -std::sin(angle), std::cos(angle);
+        for (GeometryLine & line : lines) {
+            line = line.transform(rotateMatrix);
+        }
+    }
+
+    void scale(double scl)
+    {
+        for (GeometryLine & line : lines) {
+            line.p1 *= scl;
+            line.p2 *= scl;
+        }
+    }
+
+    Vector<GraphicLine> getGraphic(cv::Size imageSize)
+    {
+        Vector<GraphicLine> graphicLines;
+        for (const GeometryLine & line : lines) {
+            graphicLines.emplace_back(line.graphic(imageSize));
+        }
+        return graphicLines;
+    }
+
+    explicit Geometry(Vector<GeometryLine> lines)
+        : lines(std::move(lines))
+    { /* empty */ }
+};
+
+enum LightBlobLabel
+{
+    LIGHT_BLOB_ARMOR_HORIZONTAL = 0,
+    LIGHT_BLOB_ARMOR_VERTICAL   = 1,
+    LIGHT_BLOB_ARM_STICK        = 2,
+    LIGHT_BLOB_OUTSIDE          = 3,
+};
+
+class PowerRuneArm
+{
+  public:
+
+    enum State
+    {
+        DEACTIVATED = 0,
+        ACTIVATING = 1,
+        ACTIVATED = 2,
+    };
+
+  private:
+
+    const Geometry geometry = Geometry({
+        GeometryLine(-13, 85, 13, 85,   LIGHT_BLOB_ARMOR_HORIZONTAL),
+        GeometryLine(-14, 68, 14, 68,   LIGHT_BLOB_ARMOR_HORIZONTAL),
+        GeometryLine(-13, 85, -14, 68,  LIGHT_BLOB_ARMOR_VERTICAL),
+        GeometryLine(13, 85, 14, 68,    LIGHT_BLOB_ARMOR_VERTICAL),
+        GeometryLine(0, 20, 0, 66,      LIGHT_BLOB_ARM_STICK),
+        GeometryLine(-6, 18, 6, 18,     LIGHT_BLOB_OUTSIDE),  // `_`
+        GeometryLine(6, 18, 16, 31,     LIGHT_BLOB_OUTSIDE),  // `/`
+        GeometryLine(-6, 18, -16, 31,   LIGHT_BLOB_OUTSIDE),  // `\`
+        GeometryLine(16, 31, 14, 68,    LIGHT_BLOB_OUTSIDE),  // `|`
+        GeometryLine(-16, 31, -14, 68,  LIGHT_BLOB_OUTSIDE),  // `|`
+    });
+
+    State state = DEACTIVATED;
+
+  public:
+
+    int id = 0;
+
+    bool isDeactivated() const
+    {
+        return state == DEACTIVATED;
+    }
+
+    bool isActivated() const
+    {
+        return state == ACTIVATED;
+    }
+
+    void light()
+    {
+        state = ACTIVATING;
+    }
+
+    void activate()
+    {
+        state = ACTIVATED;
+    }
+
+    void deactivate()
+    {
+        state = DEACTIVATED;
+    }
+
+    Geometry getGeometry(double angle = 0, double scale = 1) const
+    {
+        double rotateAngle = this->id * CV_PI * (2.0 / 5.0) + angle;
+        Geometry geo = this->geometry;
+        geo.rotate(rotateAngle);
+        geo.scale(scale);
+        return geo;
+    }
+
+    PowerRuneArm() = default;
+
+    PowerRuneArm(const PowerRuneArm & arm)
+    {
+        this->id = arm.id;
+        this->state = arm.state;
+    }
+
+};
+
+class ClockSin
+{
+  private:
+
+    Timestamp lastTimeStamp = SysClock::now();
+    // t: sec
+    double t = 0;
+
+  public:
+
+    double A, w, b;
+    ClockSin(double a, double w, double b) : A(a), w(w), b(b) {}
+
+    double operator() ()
+    {
+        Timestamp current = SysClock::now();
+        this->t += Duration<double>(current - lastTimeStamp).count();
+        double value = A * std::sin(w * t) + b;
+        lastTimeStamp = current;
+        return value;
+    }
+
+    double integral()
+    {
+        Timestamp current = SysClock::now();
+        double deltaT = Duration<double>(current - lastTimeStamp).count();
+        t += deltaT;
+        double value = (A * std::sin(w * t) + b) * deltaT;
+        lastTimeStamp = current;
+        return value;
+    }
+};
+
+enum Color
+{
+    RED,
+    BLUE,
+};
+
+class PowerRune
+{
+  private:
+
+    PowerRuneArm arms[5];
+    std::mt19937 mt = std::mt19937(std::random_device()());
+    int activeOrder[5] = { 0, 1, 2, 3, 4 };
+    int activating = 0;
+    double angle = 0;
+    double scale = 3;
+    ClockSin clockSin;
+    Color color;
+
+    double next()
+    {
+        this->angle += clockSin.integral();
+    }
+
+  public:
+
+    cv::Mat renderImage()
+    {
+        this->next();
+        auto imageSize = cv::Size((int) (scale * 200), (int) (scale * 200));
+        cv::Mat image = cv::Mat::zeros(imageSize, CV_8UC3);
+        cv::Scalar rgbColor = this->color == RED ? cv::Scalar(0, 0, 255) : cv::Scalar(255, 0, 0);
+
+        for (const auto & arm : arms) {
+            auto geo = arm.getGeometry(this->angle, this->scale);
+            for (GraphicLine line : geo.getGraphic(image.size())) {
+                switch (line.label) {
+                    case LIGHT_BLOB_ARMOR_HORIZONTAL: {
+                        if (!arm.isDeactivated()) {
+                            cv::line(image, line.p1, line.p2, rgbColor, 4, cv::LINE_AA);
+                        }
+                    } break;
+
+                    case LIGHT_BLOB_ARMOR_VERTICAL: {
+                        if (!arm.isDeactivated()) {
+                            cv::line(image, line.p1, line.p2, rgbColor, 3, cv::LINE_AA);
+                        }
+                    } break;
+
+                    case LIGHT_BLOB_ARM_STICK: {
+                        if (!arm.isDeactivated()) {
+                            cv::line(image, line.p1, line.p2, rgbColor, 10, cv::LINE_AA);
+                        }
+                    } break;
+
+                    case LIGHT_BLOB_OUTSIDE: {
+                        if (arm.isActivated()) {
+                            cv::line(image, line.p1, line.p2, rgbColor, 2, cv::LINE_AA);
+                        }
+                    } break;
+                }
+            }
+        }
+        return image;
+    }
+
+    void switchColor()
+    {
+        if (this->color == RED) {
+            this->color = BLUE;
+        } else {
+            this->color = RED;
+        }
+    }
+
+    void setSin_A(double value)
+    {
+        this->clockSin.A = value;
+    }
+
+    void setSin_w(double value)
+    {
+        this->clockSin.w = value;
+    }
+
+    void setSin_b(double value)
+    {
+        this->clockSin.b = value;
+    }
+
+    void activateNext()
+    {
+        if (activating == 6) {
+            std::shuffle(activeOrder, activeOrder + 5, mt);
+            for (auto & arm : arms) {
+                arm.deactivate();
+            }
+            activating = 0;
+            arms[activeOrder[0]].light();
+        } else {
+            arms[activeOrder[activating++]].activate();
+            if (activating == 5) {
+                activating += 1;
+            } else {
+                arms[activeOrder[activating]].light();
+            }
+        }
+    }
+
+    explicit PowerRune(double A, double w, double b, Color color)
+        noexcept : clockSin(A, w, b), color(color)
+    {
+        for (int i = 0; i < 5; i++) {
+            arms[i].id = i;
+        }
+        std::shuffle(activeOrder, activeOrder + 5, mt);
+        arms[activeOrder[activating]].light();
+    }
+
+};
+
+double bloomFactor = 1.2;
+
+cv::Mat bloom(const cv::Mat & src)
+{
+    cv::Mat gaussian, image;
+    cv::GaussianBlur(src, image, cv::Size(3, 3), 1);
+    cv::GaussianBlur(image, gaussian, cv::Size(33, 33), 10);
+    return image + gaussian * bloomFactor;
+}
+
+PowerRune powerRune(0, 0, -5, RED);
+
+void onMouseClick(int event, int, int, int, void*)
+{
+    if (event == cv::EVENT_LBUTTONDOWN) {
+        powerRune.activateNext();
+    }
+}
+
+void onColorChange(int, void*)
+{
+    powerRune.switchColor();
+}
+
+void onBloomChange(int value, void*)
+{
+    bloomFactor = 0.5 + value / 50.0;
+}
+
+void onSpeedChange(int value, void*)
+{
+    powerRune.setSin_A(value / 200.0);
+}
+
+void onIntervalChange(int value, void*)
+{
+    powerRune.setSin_w(value / 200.0);
+}
+
+void onSpeedOffsetChange(int value, void*)
+{
+    powerRune.setSin_b(value / 100.0 - 5);
+}
+
+void initializeWindow()
+{
+    cv::namedWindow("PowerRune");
+    cv::setMouseCallback("PowerRune", onMouseClick);
+    cv::createTrackbar("color", "PowerRune", nullptr, 1, onColorChange);
+    cv::createTrackbar("bloom", "PowerRune", nullptr, 100, onBloomChange);
+    cv::createTrackbar("speed", "PowerRune", nullptr, 1000, onSpeedChange);
+    cv::createTrackbar("interval", "PowerRune", nullptr, 1000, onIntervalChange);
+    cv::createTrackbar("soffset", "PowerRune", nullptr, 1000, onSpeedOffsetChange);
+}
+
+[[noreturn]]
+int main()
+{
+    initializeWindow();
+    while (true) {
+        cv::Mat frame = powerRune.renderImage();
+        cv::imshow("PowerRune", bloom(frame));
+        cv::waitKey(1);
+    }
+}
